d55539a92711e64733a4e99f9045b089c4fedc30
luanacostav <costavasconcelosluana@gmail.com>
2025-08-29 16:42:05 -0300

    Add front

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
